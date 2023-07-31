0ff96656f196d718a3ff79863a6b982dad84cc9e
Mike Zuev <zuevmihail128@gmail.com>
2023-07-31 21:03:10 +0300

    created helloController

---
+++ src/main/java/ru/mike/astonjenkins/controller/controllerHello.java
@@ -0,0 +1,19 @@
+package ru.mike.astonjenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controllerHello {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+
+    @GetMapping("/helloall")
+    public String helloAll() {
+        return "Hello everyone!!!";
+    }
+}
+

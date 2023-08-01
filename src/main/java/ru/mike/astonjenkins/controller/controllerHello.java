fcc2f428d941e07e8bb174a4035a37d1666b0c3b
Mike Zuev <zuevmihail128@gmail.com>
2023-08-01 17:10:26 +0300

    changed hello method

--- src/main/java/ru/mike/astonjenkins/controller/controllerHello.java
+++ src/main/java/ru/mike/astonjenkins/controller/controllerHello.java
@@ -8,7 +8,7 @@ public class controllerHello {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World";
+        return "Hello World and People";
     }
 
     @GetMapping("/helloall")
